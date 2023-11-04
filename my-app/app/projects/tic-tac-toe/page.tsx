c82d74997326cbcda169c155a267bff2d8e0640d
aayushvirkar <51874905+aayushvirkar@users.noreply.github.com>
2023-11-05 03:29:22 +0530

    tic tac toe completion somewhat

--- my-app/app/projects/tic-tac-toe/page.tsx
+++ my-app/app/projects/tic-tac-toe/page.tsx
@@ -1,23 +1,72 @@
+"use client";
 import Square from "@/components/square";
+import { useState } from "react";
 
 export default function TicTacToe() {
+  const [squares, setSquares] = useState(["", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState(true);
+  const [winner, setWinner] = useState<string | null>(null);
+
+  //[x,o,x,o,x]
+  function calculateWinner(squares: string[]) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        const winner = player ? "X" : "O";
+        setWinner(winner);
+      }
+    }
+  }
+
+  function handleSquares(index: number) {
+    if (squares[index]) {
+      return;
+    }
+    const newArr = [...squares];
+    newArr[index] = player ? "X" : "O";
+    setSquares(newArr);
+    setPlayer(!player);
+
+    if (newArr.filter((record) => record !== "").length >= 5) {
+      calculateWinner(newArr);
+    }
+  }
+
   return (
-    <div className="flex border-2 border-slate-300">
-      <div className="border-2 border-slate-300 ">
-        <Square content={"X"} />
-        <Square content={"X"} />
-        <Square content={"X"} />
+    <>
+      {winner ? <div>Winner is {winner}</div> : null}
+
+      <div className="flex border-2 border-slate-300">
+        <div className="border-2 border-slate-300 ">
+          <Square content={squares[0]} onSquareClick={() => handleSquares(0)} />
+          <Square content={squares[1]} onSquareClick={() => handleSquares(1)} />
+          <Square content={squares[2]} onSquareClick={() => handleSquares(2)} />
+        </div>
+        <div className="border-2 border-slate-300">
+          <Square content={squares[3]} onSquareClick={() => handleSquares(3)} />
+          <Square content={squares[4]} onSquareClick={() => handleSquares(4)} />
+          <Square content={squares[5]} onSquareClick={() => handleSquares(5)} />
+        </div>
+        <div className="border-2 border-slate-300">
+          <Square content={squares[6]} onSquareClick={() => handleSquares(6)} />
+          <Square content={squares[7]} onSquareClick={() => handleSquares(7)} />
+          <Square content={squares[8]} onSquareClick={() => handleSquares(8)} />
+        </div>
       </div>
-      <div className="border-2 border-slate-300">
-        <Square content={"O"} />
-        <Square content={"O"} />
-        <Square content={"O"} />
-      </div>
-      <div className="border-2 border-slate-300">
-        <Square content={""} />
-        <Square content={"X"} />
-        <Square content={"X"} />
-      </div>
-    </div>
+    </>
   );
 }
--- my-app/components/square.tsx
+++ my-app/components/square.tsx
@@ -1,6 +1,16 @@
-export default function Square({ content }: { content: string }) {
+export default function Square({
+  content,
+  onSquareClick,
+}: {
+  content: string;
+  onSquareClick: () => void;
+}) {
   return (
-    <div className="border-y-2 border-slate-300 p-10 hover:bg-slate-200 ">
+    <div
+      role="button"
+      className="cursor-pointer border-y-2 border-slate-300 p-10 hover:bg-slate-200"
+      onClick={onSquareClick}
+    >
       <div className="h-10 w-10 text-6xl">{content}</div>
     </div>
   );
