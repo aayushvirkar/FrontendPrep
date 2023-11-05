a41bfb2d5b3845757292de91779757846cf517a6
aayushvirkar <51874905+aayushvirkar@users.noreply.github.com>
2023-11-06 02:42:19 +0530

    Finished tic tac toe

--- my-app/app/projects/tic-tac-toe/page.tsx
+++ my-app/app/projects/tic-tac-toe/page.tsx
@@ -1,14 +1,23 @@
 "use client";
 import Square from "@/components/square";
 import { useState } from "react";
-
+import { PartyPopper, HeartHandshake } from "lucide-react";
+import { Button } from "@/components/ui/button";
 export default function TicTacToe() {
-  const [squares, setSquares] = useState(["", "", "", "", "", "", "", ""]);
-  const [player, setPlayer] = useState(true);
-  const [winner, setWinner] = useState<string | null>(null);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [squares, setSquares] = useState<(null | string)[]>([
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
+  const [currentMove, setCurrentMove] = useState(0);
 
-  //[x,o,x,o,x]
-  function calculateWinner(squares: string[]) {
+  function calculateWinner(squares: (null | string)[]) {
     const lines = [
       [0, 1, 2],
       [3, 4, 5],
@@ -26,46 +35,78 @@ export default function TicTacToe() {
         squares[a] === squares[b] &&
         squares[a] === squares[c]
       ) {
-        const winner = player ? "X" : "O";
-        setWinner(winner);
+        return squares[a];
       }
     }
+    return null;
   }
 
   function handleSquares(index: number) {
-    if (squares[index]) {
+    setCurrentMove((currentMove) => currentMove + 1);
+    const isNextPlayerX = currentMove % 2 === 0;
+
+    if (squares[index] || calculateWinner(squares)) {
       return;
     }
-    const newArr = [...squares];
-    newArr[index] = player ? "X" : "O";
-    setSquares(newArr);
-    setPlayer(!player);
+    const modifiedSquares = squares.slice();
 
-    if (newArr.filter((record) => record !== "").length >= 5) {
-      calculateWinner(newArr);
-    }
+    modifiedSquares[index] = isNextPlayerX ? "X" : "O";
+    setSquares(modifiedSquares);
+    setHistory((prevHistory) => [...prevHistory, modifiedSquares]);
+  }
+
+  const winner = calculateWinner(squares);
+  const isNextPlayerX = currentMove % 2 === 0;
+
+  let content = winner ? (
+    <span className="flex items-center justify-center">
+      Winner is <b className="mx-2">{winner}</b>{" "}
+      <PartyPopper className="mx-2 fill-orange-400" size={30} />
+    </span>
+  ) : (
+    `Next player is ${isNextPlayerX ? "X" : "O"} `
+  );
+  if (currentMove === 9 && !winner) {
+    content = (
+      <span className="flex items-center justify-center">
+        Its a Draw!{" "}
+        <HeartHandshake className="mx-2 fill-orange-400" size={30} />
+      </span>
+    );
   }
 
   return (
     <>
-      {winner ? <div>Winner is {winner}</div> : null}
-
-      <div className="flex border-2 border-slate-300">
-        <div className="border-2 border-slate-300 ">
+      <div className="m-2 text-center text-xl">{content}</div>
+      <div className="flex border-2 border-slate-400">
+        <div className="border-2 border-slate-400 ">
           <Square content={squares[0]} onSquareClick={() => handleSquares(0)} />
           <Square content={squares[1]} onSquareClick={() => handleSquares(1)} />
           <Square content={squares[2]} onSquareClick={() => handleSquares(2)} />
         </div>
-        <div className="border-2 border-slate-300">
+        <div className="border-2 border-slate-400">
           <Square content={squares[3]} onSquareClick={() => handleSquares(3)} />
           <Square content={squares[4]} onSquareClick={() => handleSquares(4)} />
           <Square content={squares[5]} onSquareClick={() => handleSquares(5)} />
         </div>
-        <div className="border-2 border-slate-300">
+        <div className="border-2 border-slate-400">
           <Square content={squares[6]} onSquareClick={() => handleSquares(6)} />
           <Square content={squares[7]} onSquareClick={() => handleSquares(7)} />
           <Square content={squares[8]} onSquareClick={() => handleSquares(8)} />
         </div>
+      </div>
+      <div className="my-8 flex max-w-[365px] flex-row flex-wrap">
+        {history.map((record, index) => {
+          return (
+            <Button
+              onClick={() => setSquares(history[index])}
+              className="my-2 ml-2"
+              size={"sm"}
+            >
+              Go to move {index}
+            </Button>
+          );
+        })}
       </div>
     </>
   );
--- my-app/components/square.tsx
+++ my-app/components/square.tsx
@@ -2,13 +2,13 @@ export default function Square({
   content,
   onSquareClick,
 }: {
-  content: string;
+  content: string | null;
   onSquareClick: () => void;
 }) {
   return (
     <div
       role="button"
-      className="cursor-pointer border-y-2 border-slate-300 p-10 hover:bg-slate-200"
+      className="cursor-pointer border-y-2 border-slate-400 p-10 hover:bg-slate-200"
       onClick={onSquareClick}
     >
       <div className="h-10 w-10 text-6xl">{content}</div>
